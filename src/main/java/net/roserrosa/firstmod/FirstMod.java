85f3abb8556404473bf9a214fdbacf7abdea7b46
rosa <xxxskulzthegamerxx@gmail.com>
2026-02-12 09:09:24 +0100

    hhh

--- src/main/java/net/roserrosa/firstmod/FirstMod.java
+++ src/main/java/net/roserrosa/firstmod/FirstMod.java
@@ -15,7 +15,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// Very important Comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FirstMod.MOD_ID)
 public class FirstMod {
