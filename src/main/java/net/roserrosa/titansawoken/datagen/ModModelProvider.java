a9a065c785d76e9a9cddca664f1e04b8d98bff09
rosa <xxxskulzthegamerxx@gmail.com>
2026-03-31 23:33:09 +0200

    bronze great-tools

--- src/main/java/net/roserrosa/titansawoken/datagen/ModModelProvider.java
+++ src/main/java/net/roserrosa/titansawoken/datagen/ModModelProvider.java
@@ -1,26 +1,21 @@
 package net.roserrosa.titansawoken.datagen;
 
 import net.minecraft.client.data.models.model.*;
+import net.minecraft.client.renderer.item.ClientItem;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.ItemModels;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.client.model.generators.template.ExtendedModelTemplateBuilder;
 import net.roserrosa.titansawoken.TitansAwoken;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
-import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
-import net.minecraft.client.renderer.item.ClientItem;
-import net.minecraft.client.renderer.item.ConditionalItemModel;
-import net.minecraft.client.renderer.item.ItemModel;
-import net.minecraft.client.renderer.item.properties.conditional.HasComponent;
 import net.minecraft.core.Holder;
-import net.minecraft.data.PackOutput;
-import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.roserrosa.titansawoken.block.ModBlocks;
 import net.roserrosa.titansawoken.item.ModItems;
-import net.roserrosa.titansawoken.util.ModItemModelGenerators;
+import net.roserrosa.titansawoken.item.custom.ScytheItem;
 
 import java.util.stream.Stream;
 
@@ -32,7 +27,7 @@ public class ModModelProvider extends ModelProvider{
     }
 
     @Override
-    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels, ModItemModelGenerators modItemModels) {
+    protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
         // ITEMS
         itemModels.generateFlatItem(ModItems.BRONZE.get(), ModelTemplates.FLAT_ITEM);
         itemModels.generateFlatItem(ModItems.BRONZE_NUGGET.get(), ModelTemplates.FLAT_ITEM);
@@ -50,8 +45,7 @@ public class ModModelProvider extends ModelProvider{
         itemModels.generateSpear(ModItems.BRONZE_SPEAR.get());
         itemModels.generateSpear(ModItems.BRONZE_GREATAXE.get());
         itemModels.generateSpear(ModItems.BRONZE_SPADE.get());
-        modItemModels.generateScythe(ModItems.BRONZE_SCYTHE.get(), ModelTemplates.FLAT_ITEM.extend()
-                .);
+        generateScythe(ModItems.BRONZE_SCYTHE.get(), ModelTemplates.FLAT_ITEM.extend()
 
         // BLOCKS
         blockModels.createTrivialCube(ModBlocks.BRONZE_BLOCK.get());
@@ -62,6 +56,12 @@ public class ModModelProvider extends ModelProvider{
 
         blockModels.createTrivialCube(ModBlocks.RAW_TIN_BLOCK.get());
 
+    }
+
+    public void generateScythe(ScytheItem item, ExtendedModelTemplateBuilder modelTemplate,  ItemModelGenerators itemModels) {
+        ItemModel.Unbaked itemmodel$unbaked = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, ModelTemplates.FLAT_ITEM));
+        ItemModel.Unbaked itemmodel$unbaked1 = ItemModelUtils.plainModel(ModelTemplates.SPEAR_IN_HAND.create(item, TextureMapping.layer0(TextureMapping.getItemTexture(item, "_in_hand")), itemModels.modelOutput));
+        itemModels.itemModelOutput.accept(item, createFlatModelDispatch(itemmodel$unbaked, itemmodel$unbaked1), new ClientItem.Properties(true, false, 1.95F));
     }
 
     @Override
