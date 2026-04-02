486ee83aeec653865b72404bd0d7a7bb6227a665
rosa <xxxskulzthegamerxx@gmail.com>
2026-04-02 22:34:00 +0200

    bronze great-tools

--- src/main/java/net/roserrosa/titansawoken/datagen/ModModelProvider.java
+++ src/main/java/net/roserrosa/titansawoken/datagen/ModModelProvider.java
@@ -1,10 +1,12 @@
 package net.roserrosa.titansawoken.datagen;
 
 import net.minecraft.client.data.models.model.*;
+import net.minecraft.client.renderer.block.model.ItemModelGenerator;
 import net.minecraft.client.renderer.item.ClientItem;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.item.ItemModels;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.neoforged.neoforge.client.model.generators.template.ExtendedModelTemplateBuilder;
 import net.roserrosa.titansawoken.TitansAwoken;
 import net.minecraft.client.data.models.BlockModelGenerators;
@@ -45,7 +47,7 @@ public class ModModelProvider extends ModelProvider{
         itemModels.generateSpear(ModItems.BRONZE_SPEAR.get());
         itemModels.generateSpear(ModItems.BRONZE_GREATAXE.get());
         itemModels.generateSpear(ModItems.BRONZE_SPADE.get());
-        generateScythe(ModItems.BRONZE_SCYTHE.get(), ModelTemplates.FLAT_ITEM.extend()
+        generateScythe(ModItems.BRONZE_SCYTHE.get(), itemModels);
 
         // BLOCKS
         blockModels.createTrivialCube(ModBlocks.BRONZE_BLOCK.get());
@@ -58,11 +60,30 @@ public class ModModelProvider extends ModelProvider{
 
     }
 
+    // TEMPLATES
+
     public void generateScythe(ScytheItem item, ExtendedModelTemplateBuilder modelTemplate,  ItemModelGenerators itemModels) {
         ItemModel.Unbaked itemmodel$unbaked = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, ModelTemplates.FLAT_ITEM));
-        ItemModel.Unbaked itemmodel$unbaked1 = ItemModelUtils.plainModel(ModelTemplates.SPEAR_IN_HAND.create(item, TextureMapping.layer0(TextureMapping.getItemTexture(item, "_in_hand")), itemModels.modelOutput));
+        ItemModel.Unbaked itemmodel$unbaked1 = ItemModelUtils.plainModel(modelTemplate.build().create(item, TextureMapping.layer0(TextureMapping.getItemTexture(item, "_in_hand")), itemModels.modelOutput));
         itemModels.itemModelOutput.accept(item, createFlatModelDispatch(itemmodel$unbaked, itemmodel$unbaked1), new ClientItem.Properties(true, false, 1.95F));
     }
+
+    public void generateScythe(ScytheItem item,   ItemModelGenerators itemGenerator) {
+        this.generateScythe(item,
+                ModelTemplates.FLAT_ITEM.extend()
+                        .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, b -> b
+                                .rotation(0, -90, 55)
+                                .translation((float) 0, 4.0f, 0.5f)
+                                .scale(1.7f)
+                        )
+                        .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND, b -> b
+                                .rotation(0 , 90, 25)
+                                .translation(1.13f, 3.2f, 1.13f)
+                                .scale(1.36f)
+                        ),
+                itemGenerator);
+    }
+
 
     @Override
     protected Stream<? extends Holder<Block>> getKnownBlocks() {
